bacb0c527accf128655451cf9480ca002b23caa8
Htet Htet Hlaing <htethtetster25@gmail.com>
2026-07-04 22:08:01 +1000

    OOP Abstract Practice on 4 July 2026

---
+++ June2026.AbstractPractice/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("This is a practice of an abstract class in OOP");
+
+Dog d = new Dog("Buddy", 3);
+Cat c = new Cat("Kitty", 2);
+Console.WriteLine("This is how a Dog sounds and sleeps...");
+d.MakeSound();
+d.Sleep();
+Console.WriteLine("=====================================");
+Console.WriteLine("This is how a Cat sounds and sleeps...");
+c.MakeSound();
+c.Sleep();
+abstract class Animal
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public Animal(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public abstract void MakeSound();
+
+    public void Sleep()
+    {
+        Console.WriteLine($"{Name} is sleeping.");
+    }
+}
+
+class Dog : Animal
+{
+    public Dog(string name, int age) : base(name, age)
+    {
+
+    }
+    public override void MakeSound()
+    {
+        Console.WriteLine($"{Name} says Woof!");
+    }
+}
+
+class Cat : Animal
+{
+    public Cat(string name, int age) : base (name, age)
+    {
+
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine($"{Name} says Meow!");
+    }
+}
