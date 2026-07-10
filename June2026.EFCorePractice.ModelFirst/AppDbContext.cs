aab324ea728e2963ab2cd261e1fae0929329c206
Htet Htet Hlaing <htethtetster25@gmail.com>
2026-07-10 13:44:26 +1000

    EFCore Practice For Model First on 10 July 2026

---
+++ June2026.EFCorePractice.ModelFirst/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace June2026.EFCorePractice.ModelFirst
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                SqlConnectionStringBuilder _sb = new SqlConnectionStringBuilder
+                {
+                    DataSource = ".\\MSSQLSERVER01",
+                    InitialCatalog = "June2026",
+                    UserID = "sa",                        
+                    Password = "sasa@123",                
+                    TrustServerCertificate = true
+                };
+                optionsBuilder.UseSqlServer(_sb.ConnectionString);
+            }
+        }
+
+        public DbSet<StaffEntity> Staffs { get; set; }
+    }
+}
---
+++ June2026.EFCorePractice.ModelFirst/Program.cs
@@ -0,0 +1,65 @@
+﻿using June2026.EFCorePractice.ModelFirst;
+
+Console.WriteLine("This is an EF Core - Model First Practice.");
+
+AppDbContext db = new AppDbContext();
+var lst = db.Staffs.ToList();     //this is a LINQ method syntax.  it works the same as "Select * from Tbl_Staff"
+
+foreach(var item in lst)
+{
+    Console.WriteLine(item.Id);
+    Console.WriteLine(item.Name);
+    Console.WriteLine("-------------------------");
+}
+
+
+/***  
+ * Record Read 
+ */
+var staff2 = db.Staffs.Where(x => x.Id == 1000).FirstOrDefault();
+
+
+/***  
+ * Record Create 
+ */
+
+StaffEntity staffEntity = new StaffEntity
+{
+    Name = "Blueberry"
+};
+
+db.Staffs.Add(staffEntity);
+int result = db.SaveChanges();     // Blueberry will be saved into the database after execution this line
+
+
+
+/***  
+ * Record Update 
+ */
+var staff1 = db.Staffs.Where(x => x.Id == 1).FirstOrDefault();
+if (staff1 is null)
+{
+    Console.WriteLine("Staff not found.");
+}
+else
+{
+    staff1.Name = "Htet Htet";
+    db.SaveChanges();
+}
+
+
+/*** 
+ * Record Delete
+ */
+
+var staff3 = db.Staffs.Where(x => x.Id == 1).FirstOrDefault();
+if (staff3 is null)
+{
+    Console.WriteLine("Staff not found.");
+    return;
+}
+db.Staffs.Remove(staff3);
+db.SaveChanges();
+
+
+Console.ReadLine();
